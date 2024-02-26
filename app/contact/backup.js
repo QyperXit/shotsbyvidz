612b042b4932c31a50bd926ee460817a603d298d
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-02-26 20:48:06 +0000

    contact page POSTS

--- app/contact/backup.js
+++ app/contact/backup.js
@@ -5,37 +5,33 @@ import cat from "../../public/images/cat.jpg";
 
 const Contact = () => {
   return (
-    <div className="flex-col flex justify-center lg:flex-row px my-32 gap-24 items-center my-auto">
-      <div className="w-fit max-w-[35em] bg-black">
+    <div className="  flex-col  flex justify-center  lg:flex-row px  my-32  gap-24 items-center my-auto">
+      <div className=" w-fit  max-w-[35em] bg-black">
         <Image
           src={cat}
           width={1080}
           height={1350}
-          alt="cat picture"
+          alt="cat pictu"
           className="object-contain"
           placeholder="blur"
         />
       </div>
-      <div className="max-w-md">
-        <p className="text-sm text-center text-white">- CONTACT</p>
-        <h2 className="text-4xl text-white mb-4 text-center font-sans font-normal mt-7">
+      <div className="max-w-md ">
+        <p className=" text-sm text-center  text-white ">- CONTACT</p>
+        <h2 className="text-4xl  text-white mb-4 text-center   font-sans font-normal mt-7">
           Get in touch
         </h2>
-        <p className="text-center text-amber-400 mt-7">Shaunlm@live.co.uk</p>
-        <form
-          action="https://formsubmit.co/chaun.online@gmail.com"
-          method="POST"
-          className="space-y-8 mt-7"
-        >
+        <p className="  text-center  text-amber-400 mt-7">Shaunlm@live.co.uk</p>
+        <form className="space-y-8 mt-7">
           <div className="flex space-x-4">
             <div>
-              <label htmlFor="firstName" className="block text-white">
+              <label htmlFor="firstName" className="block  text-white">
                 Name*
               </label>
               <input
                 type="text"
                 id="firstName"
-                name="name"
+                name="firstName"
                 placeholder="Enter your first name"
                 className="w-full border bg-white mt-auto rounded py-3 px-3 focus:outline-none focus:border-amber-500"
                 required
@@ -45,7 +41,7 @@ const Contact = () => {
               <input
                 type="text"
                 id="lastName"
-                name="last_name"
+                name="lastName"
                 placeholder="Last Name"
                 className="w-full border bg-white rounded py-3 px-3 focus:outline-none focus:border-amber-500"
                 required
@@ -53,7 +49,7 @@ const Contact = () => {
             </div>
           </div>
           <div>
-            <label htmlFor="email" className="block text-white">
+            <label htmlFor="email" className="block  text-white">
               Email*
             </label>
             <input
@@ -66,7 +62,7 @@ const Contact = () => {
             />
           </div>
           <div>
-            <label htmlFor="message" className="block text-white">
+            <label htmlFor="message" className="block  text-white">
               Message*
             </label>
             <textarea
@@ -80,7 +76,7 @@ const Contact = () => {
           </div>
           <button
             type="submit"
-            className="bg-[#860C0C] text-white py-5 px-10 hover:bg-[#880c0c8e] transition duration-200"
+            className="bg-[#860C0C] text-white py-5 px-10  hover:bg-[#880c0c8e]  transition duration-200"
           >
             Submit
           </button>
