df76c7f00aa3b8ad2f00fca3322cde30eb13c740
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-02-28 01:06:32 +0000

    added local images

--- public/blogPosts/data.js
+++ public/blogPosts/data.js
@@ -1,80 +1,80 @@
 export const items = [
   {
     id: 1,
-    url: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-    title: "Whispers of the Dawn",
+    url: "/blogPosts/manzil.jpg",
+    title: "Product photography Ideas",
     description:
       "As the sun gently rises, casting its golden hues upon the land, nature awakens to greet the new day with a symphony of color and song.",
     tags: ["Mountains", "Aesthetic", "Pretty", "Scenic", "Rugged"],
   },
   {
     id: 2,
-    url: "https://images.unsplash.com/photo-1570641963303-92ce4845ed4c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    title: "Echoes of Serenity",
+    url: "/blogPosts/tapwater.jpg",
+    title: "Church Stretton Camp & Fish",
     description:
       "Amidst towering peaks and serene valleys, where the air is crisp and pure, lies a tranquil haven of peace and beauty waiting to be discovered.",
     tags: ["Mountains", "Aesthetic", "Pretty", "Scenic", "Rugged"],
   },
   {
     id: 3,
-    url: "https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-    title: "Silent Whispers of the Forest",
+    url: "/blogPosts/church.jpg",
+    title: "A Church next to a School...",
     description:
       "In the heart of the ancient woods, where the trees whisper secrets of old, one can find solace and connection amidst nature's timeless embrace.",
     tags: ["Mountains", "Aesthetic", "Pretty", "Scenic", "Rugged"],
   },
   {
     id: 4,
-    url: "https://images.unsplash.com/photo-1614597396930-cd6760b99f7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    title: "Whispers of the Sea",
+    url: "/blogPosts/drone.jpg",
+    title: "Dji mini 2",
     description:
       "As waves gently caress the shore and seagulls dance in the sky, the ocean sings a melodic lullaby, inviting all to surrender to its eternal rhythm.",
     tags: ["Mountains", "Aesthetic", "Pretty", "Scenic", "Rugged"],
   },
   {
     id: 5,
-    url: "https://images.unsplash.com/34/BA1yLjNnQCI1yisIZGEi_2013-07-16_1922_IMG_9873.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80",
-    title: "Whispers of the Meadow",
+    url: "/blogPosts/horse.jpg",
+    title: "Deer Park Farm",
     description:
       "In fields of wildflowers that sway in the gentle breeze, one can hear the soft whispers of the meadow, a testament to the beauty and resilience of nature.",
     tags: ["Mountains", "Aesthetic", "Pretty", "Scenic", "Rugged"],
   },
   {
     id: 6,
-    url: "https://images.unsplash.com/photo-1507629221898-576a56b530bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
-    title: "Whispers of the Twilight",
+    url: "/blogPosts/churchClock.jpg",
+    title: "Black & White (Color Calibration)",
     description:
       "As the day fades into night and the sky is painted with hues of pink and gold, the world is enveloped in a gentle hush, a whispered promise of dreams to come.",
     tags: ["Mountains", "Aesthetic", "Pretty", "Scenic", "Rugged"],
   },
   {
     id: 7,
-    url: "https://images.unsplash.com/photo-1662541109467-a80473ab73e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    title: "Whispers of the Tides",
+    url: "/blogPosts/fire.jpg",
+    title: "Lantern",
     description:
       "As the tide ebbs and flows, carrying secrets from distant shores, the ocean whispers tales of love, loss, and endless adventure, beckoning all to its boundless depths.",
     tags: ["Mountains", "Aesthetic", "Pretty", "Scenic", "Rugged"],
   },
   {
     id: 8,
-    url: "https://images.unsplash.com/photo-1662544954782-4c137c9b34a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
-    title: "Whispers of the Cosmos",
+    url: "/blogPosts/lakeSign.jpg",
+    title: "Engine Pool",
     description:
       "In the vast expanse of space, where stars twinkle like diamonds in the velvet night sky, the universe whispers secrets of creation and the mysteries of existence.",
     tags: ["Mountains", "Aesthetic", "Pretty", "Scenic", "Rugged"],
   },
   {
     id: 9,
-    url: "https://images.unsplash.com/photo-1659536009108-ebe9053222d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    title: "Whispers of the Night",
+    url: "/blogPosts/swan.jpg",
+    title: "Swan",
     description:
       "As darkness descends and the stars emerge to illuminate the heavens, the night whispers secrets known only to those who dare to listen, revealing the beauty of the unseen.",
     tags: ["Mountains", "Aesthetic", "Pretty", "Scenic", "Rugged"],
   },
   {
     id: 10,
-    url: "https://images.unsplash.com/photo-1662532577856-e8ee8b138a8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1456&q=80",
-    title: "Whispers of the Winds",
+    url: "/blogPosts/redlight.jpg",
+    title: "Red Light",
     description:
       "In the rustle of leaves and the gentle sway of branches, one can hear the whispers of the winds, carrying messages from distant lands and tales of adventure untold.",
     tags: ["Mountains", "Aesthetic", "Pretty", "Scenic", "Rugged"],
