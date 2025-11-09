ee45b487ac90c5ec7b0bbff19b254e7b68963a64
QyperXit <67711154+QyperXit@users.noreply.github.com>
2025-11-09 21:36:08 +0000

    refactor: adjust gallery styling in List component, improve layout consistency

--- app/automotive/List.tsx
+++ app/automotive/List.tsx
@@ -14,14 +14,15 @@ const List: React.FC<{
   setSelected: React.Dispatch<React.SetStateAction<ItemType | null>>;
 }> = ({ setSelected }) => {
   const loader = useCallback(
-    () => import("../../public/automotive/auto").then((module) => module.default),
-    [],
+    () =>
+      import("../../public/automotive/auto").then((module) => module.default),
+    []
   );
   const items = useGalleryData<ItemType[]>("automotive", loader) ?? [];
 
   return (
     <div className="bg-black p-4 px-[min(5vw,20em)]">
-      <div className="columns-1 sx:columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-5 bg-black max-w-screen-xl mx-auto">
+      <div className="max-w-screen-xl gap-5 mx-auto bg-black columns-1 sx:columns-2 md:columns-3 lg:columns-4 ">
         {items.map((item) => (
           <GalleryCard
             key={item.id}
